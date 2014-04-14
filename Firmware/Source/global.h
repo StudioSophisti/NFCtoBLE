731a302cf9a45d6c066b3f8e6d6d195a1c440579
Tijn Kooijmans <tijn@studiosophisti.nl>
2014-04-14 11:45:11 +0200

    fixed uuid in firmware

--- Firmware/Source/global.h
+++ Firmware/Source/global.h
@@ -3,7 +3,7 @@
 
 // Base 128-bit UUID: 3F29121C-XXXX-000B-0001-000000000000
 #define BASE_UUID_128( uuid )  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, \
-        0x0A, 0x00, LO_UINT16( uuid ), HI_UINT16( uuid ), 0x1C, 0x12, 0x29, 0x3F
+        0x0B, 0x00, LO_UINT16( uuid ), HI_UINT16( uuid ), 0x1C, 0x12, 0x29, 0x3F
                                     
                                     
 #endif //GLOBAL_H
